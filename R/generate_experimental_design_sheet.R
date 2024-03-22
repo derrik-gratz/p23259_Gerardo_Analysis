e7abbfe13b684be72be3dfd1967f219cf9b93d2a
derrik-gratz <derrik.gratz@gmail.com>
2024-03-22 20:58:42 +0000

    fix mislabeled sample

--- R/generate_experimental_design_sheet.R
+++ R/generate_experimental_design_sheet.R
@@ -9,6 +9,7 @@ files <- dir(data_dir, recursive = FALSE,
                include.dirs = TRUE, pattern = 'p[0-9]+.*')
 samples <- str_extract(files, 's[0-9]+')
 individual <- str_extract(files, 'Z[0-9]+')
+individual[individual=='Z15229'] <- 'Z15228'
 date <- str_extract(files, '[0-9]{2}-[0-9]{2}-[0-9]{2}')
 tissue <- ifelse(grepl('Rectal-Biopsy', files, fixed=TRUE), 'rectal-biopsy', 'PBMC')
 
